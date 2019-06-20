4904d3e7c5c02d6c2e848273f6092a97eac5c8d8
Ian Walker <iantdwalker@gmail.com>
2019-06-20 15:44:56 +0100

    Inserted the nested component ArtistComponent into the application in place of the hard-coded template and style for the ArtistSearchComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { SessionComponent } from './session/session.component';
 import { ArtistSearchComponent } from './artist-search/artist-search.component';
 import { RelatedArtistsComponent } from './related-artists/related-artists.component';
+import { ArtistComponent } from './shared/components/artist/artist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SessionComponent,
     ArtistSearchComponent,
-    RelatedArtistsComponent
+    RelatedArtistsComponent,
+    ArtistComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shared/components/artist/artist.component.ts
+++ src/app/shared/components/artist/artist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { SpotifyService } from '../../../shared/services/spotify.service';
 import { IArtist } from '../../../shared/model/Artist/artist';
 
@@ -7,13 +7,18 @@ import { IArtist } from '../../../shared/model/Artist/artist';
     templateUrl: './artist.component.html',
     styleUrls: ['./artist.component.css']
 })
-export class ArtistComponent implements OnInit {
-    artist: IArtist;
+export class ArtistComponent implements OnInit, OnChanges {
+    @Input() artist: IArtist;
     
     constructor(private _spotifyService: SpotifyService) {
     }
 
     ngOnInit(): void {
         // init code can go here
-    }    
+    }
+    
+    ngOnChanges(): void {
+        // container change code can go here - such as when the artists-search comp sets a new selectedArtist
+        //console.log('ngOnChanges fired');
+    }
 }
