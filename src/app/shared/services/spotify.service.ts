79f7a4c190844fe6acfdc3c2eea9538c7c52c76f
Ian Walker <iantdwalker@gmail.com>
2020-01-14 19:09:39 +0000

    WIP for 'General - RxJs improvements'. Neatened up the pipe operators in the Spotify Service functions. Removed maps not required and switched to tap for the one that is required but doesn't modifyu the output stream.

--- src/app/shared/services/spotify.service.ts
+++ src/app/shared/services/spotify.service.ts
@@ -19,53 +19,45 @@ export class SpotifyService {
     }
 
     getClientCredentialsAccessToken(): Observable<ISpotifyAccessToken> {
-        return this.http.get<ISpotifyAccessToken>(this._clientCredentialsAccessTokenUrl).pipe(
-            map(response => {
-                this._spotifyAccessToken = response;
-                return response;
-            }),
-            catchError(this.handleError));
+        return this.http.get<ISpotifyAccessToken>(this._clientCredentialsAccessTokenUrl)
+        .pipe(
+            tap(response => this._spotifyAccessToken = response),
+            catchError(error => this.handleError(error))
+        );
     }
 
     getArtists(artistSearchTerm: string): Observable<ISearchedArtists> {
         artistSearchTerm = artistSearchTerm.trim();
-
         const httpParams = new HttpParams()
             .set('q', artistSearchTerm)
             .set('type', 'artist')
             .set('limit', '5');
-
         const httpHeaders = new HttpHeaders()
             .set('Authorization', 'Bearer ' + this._spotifyAccessToken.access_token);
-
         const httpOptions = {
             headers: httpHeaders,
             params: httpParams
         };
 
-        return this.http.get<ISearchedArtists>(this._spotifySearchUrl, httpOptions).pipe(
-            map(response => {
-                return response;
-            }),
-            catchError(this.handleError));
+        return this.http.get<ISearchedArtists>(this._spotifySearchUrl, httpOptions)
+        .pipe(
+            catchError(error => this.handleError(error))
+        );
     }
 
     getRelatedArtists(artistId: string): Observable<IRelatedArtists> {
         artistId = artistId.trim();
         const getRelatedArtistsUrl = this._spotifyRelatedArtistsUrl.replace('{id}', artistId);
-
         const httpHeaders = new HttpHeaders()
             .set('Authorization', 'Bearer ' + this._spotifyAccessToken.access_token);
-
         const httpOptions = {
             headers: httpHeaders
         };
 
-        return this.http.get<IRelatedArtists>(getRelatedArtistsUrl, httpOptions).pipe(
-            map(response => {
-                return response;
-            }),
-            catchError(this.handleError));
+        return this.http.get<IRelatedArtists>(getRelatedArtistsUrl, httpOptions)
+        .pipe(
+            catchError(error => this.handleError(error))
+        );
     }
 
     private handleError(error: HttpErrorResponse) {
