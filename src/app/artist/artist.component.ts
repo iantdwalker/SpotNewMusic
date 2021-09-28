503fe7015014089585ccb08cf24769644fdc5b14
Ian <iantdwalker@gmail.com>
2021-09-28 20:17:50 +0100

    Removed rootDir src and added a full path for artist

--- src/app/artist/artist.component.ts
+++ src/app/artist/artist.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { IArtist } from 'src/app/shared/model/artist/artist';
+import { IArtist } from '@artist/artist';
 
 @Component ({
     selector: 'app-artist',
