b5bb7fff5c81e77694bf4a422a07125ac88c2786
Ian <iantdwalker@gmail.com>
2022-11-19 10:53:34 +0000

    Removed redundant export of the heading component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
 import { SessionComponent } from '@session/session.component';
@@ -33,7 +32,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
-  exports: [HeadingComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
